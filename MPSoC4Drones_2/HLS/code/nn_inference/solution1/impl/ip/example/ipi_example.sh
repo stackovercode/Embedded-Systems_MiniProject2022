8a29123e09d190d1dc83574057806c8934d51b4d
Steffen Waage Jensen <90548875+SteffenWJ@users.noreply.github.com>
2023-01-15 22:26:33 +0100

    Delete MPSoC4Drones_2 directory
    Double

--- MPSoC4Drones_2/HLS/code/nn_inference/solution1/impl/ip/example/ipi_example.sh
+++
@@ -1,6 +0,0 @@
-# ==============================================================
-# Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2020.2 (64-bit)
-# Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
-# ==============================================================
-
-/home/steffen/tools/Vivado/2020.2/bin/vivado  -notrace -mode batch -source ipi_example.tcl -tclargs xczu3eg-sbva484-1-i ../xilinx_com_hls_nn_inference_1_0.zip
