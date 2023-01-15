0cb059104b3550e600290e1a0084e5587595a047
Steffen Waage Jensen <90548875+SteffenWJ@users.noreply.github.com>
2023-01-15 22:33:14 +0100

    Add files via upload

---
+++ VITIS/emp_bare_code/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ /home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xparameters.h \
+ /home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xparameters_ps.h \
+ /home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xil_cache.h \
+ /home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xil_types.h \
+ ../src/platform_config.h
+
+/home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xparameters.h:
+
+/home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+/home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xil_cache.h:
+
+/home/steffen/emb_my/MPSoC4Drones/VITIS_my/emb_system/export/emb_system/sw/emb_system/standalone_domain/bspinclude/include/xil_types.h:
+
+../src/platform_config.h:
