9631e3c9aea6645b3a0cef683d727c76e70c0464
SteffenWJ <steffenwaagejensen@gmail.com>
2023-01-15 22:17:21 +0100

    test3

--- MPSoC4Drones_2/VITIS/emb_system/zynqmp_fsbl/zynqmp_fsbl_bsp/psu_cortexa53_0/lib/translation_table.d
+++
@@ -1,9 +0,0 @@
-../../../lib/translation_table.o: translation_table.S \
- ../../../include/xparameters.h ../../../include/xparameters_ps.h \
- bspconfig.h
-
-../../../include/xparameters.h:
-
-../../../include/xparameters_ps.h:
-
-bspconfig.h:
--- MPSoC4Drones_2/VITIS/emb_system/zynqmp_fsbl/zynqmp_fsbl_bsp/psu_cortexa53_0/lib/xil_smc.d
+++
@@ -1,7 +0,0 @@
-../../../lib/xil_smc.o: xil_smc.c xil_types.h xil_smc.h bspconfig.h
-
-xil_types.h:
-
-xil_smc.h:
-
-bspconfig.h:
